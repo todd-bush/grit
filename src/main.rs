bf2098003c47f8db9d78843ee7bba33316a0d4be
Todd Bush <todd.bush1471@gmail.com>
2020-03-28 19:35:31 -0400

    fixed typo

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ Grit.
 
 Usage:
     grit fame [--branch=<string>] [--sort=<field>] [--debug]
-    grit bydate [--branch=<string>] [--start-date=<string>] [--end-date=<string>] [--file=<string>] [--image][--debug]
+    grit bydate [--branch=<string>] [--start-date=<string>] [--end-date=<string>] [--file=<string>] [--image] [--debug]
 
 Command:
     fame: produces counts by commit author
@@ -53,7 +53,7 @@ Options:
     --start-date=<string>       start date for bydate in YYYY-MM-DD format.
     --end-date=<string>         end date for bydate in YYYY-MM-DD format.
     --file=<string>             output file for the by date file.  Sends to stdout by default
-    --test_image                creates an image for the by_date graph.  file is required
+    --image                creates an image for the by_date graph.  file is required
     --verbose
 ";
 
